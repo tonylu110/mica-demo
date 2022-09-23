c525fb7bf0d317e39e0eed673f08a050b3a92317
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-09-23 10:01:43 +0800

    Add: mica style file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import { createApp } from 'vue'
 import './style.css'
+import './mica.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
