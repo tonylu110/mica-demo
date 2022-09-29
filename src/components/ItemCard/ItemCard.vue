54a510d84972ace37855a6f6c50015dad70a1ecb
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-09-29 13:45:14 +0800

    Add: item card component

---
+++ src/components/ItemCard/ItemCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="item-card">
+    <img :src="img" alt="">
+    <span class="title">{{ title }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  img: {
+    default: './images/win11.jpeg',
+    type: String
+  },
+  title :{
+    default: 'title',
+    type: String
+  }
+})
+</script>
+
+<style scoped>
+.item-card {
+  background-color: #ffffff70;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 70px;
+  width: 270px;
+  overflow: hidden;
+  border-radius: 8px;
+  margin: 10px;
+  padding-right: 15px;
+  border: 1px solid #dddddd60;
+}
+
+.item-card:hover {
+  box-shadow: 0 1px 5px #00000035;
+}
+
+img {
+  margin-right: 15px;
+  height: 70px;
+  width: 70px;
+  object-fit: cover;
+}
+
+.title {
+  font-weight: bold;
+}
+
+@media (prefers-color-scheme: dark) {
+  .item-card {
+    background-color: #33333355;
+    border: 1px solid #33333340;
+  }
+}
+</style>
--- src/pages/Fold/Fold.vue
+++ src/pages/Fold/Fold.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    Fold
-  </div>
+  <ScrollY>
+    <span class="title">Fold</span>
+    <div class="items">
+      <ItemCard title="page1" @click="router.push({name: 'page1'})"/>
+      <ItemCard title="page2" @click="router.push({name: 'page2'})"/>
+    </div>
+  </ScrollY>
 </template>
 
 <script setup lang="ts">
+import ItemCard from '../../components/ItemCard/ItemCard.vue';
+import ScrollY from '../../components/ScrollView/ScrollY.vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
 </script>
 
 <style scoped>
+.title {
+  margin-top: 40px;
+  display: block;
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: bold;
+}
 
+.items {
+  width: calc(100% - 80px);
+  margin-left: 40px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 </style>
